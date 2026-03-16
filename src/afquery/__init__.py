2103b99b89930ee1578e5cc0c6685cf234dbf9c1
Daniel López López <d.lopez.bioinfo@gmail.com>
2026-03-16 14:41:21 +0100

    feat: add versioning and release automation

--- src/afquery/__init__.py
+++ src/afquery/__init__.py
@@ -1,4 +1,5 @@
+from afquery._version import __version__
 from .database import Database
 from .models import QueryResult
 
-__all__ = ["Database", "QueryResult"]
+__all__ = ["Database", "QueryResult", "__version__"]
