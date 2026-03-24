7143dcae20a325d8f62f9bf21e510af327e5903c
Daniel López López <d.lopez.bioinfo@gmail.com>
2026-03-24 12:34:46 +0100

    feat: expose variant_info in Database public API

--- src/afquery/__init__.py
+++ src/afquery/__init__.py
@@ -1,5 +1,6 @@
 from afquery._version import __version__
 from .database import Database
-from .models import QueryResult, AfqueryWarning
+from .models import QueryResult, SampleCarrier, AfqueryWarning
+from .variant_info import variant_info
 
-__all__ = ["Database", "QueryResult", "AfqueryWarning", "__version__"]
+__all__ = ["Database", "QueryResult", "SampleCarrier", "AfqueryWarning", "variant_info", "__version__"]
